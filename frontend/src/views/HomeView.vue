905c0bae32e4051c80c9b6ca9058d01efc81599e
Milie10 <elaye78370@gmail.com>
2024-10-06 23:40:45 +0200

    appel du endpoint creation tache dans le front

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,15 @@
 <script setup>
 import HelloWorld from '../components/HelloWorld.vue'
 import Form from "../components/FormTask.vue"
+import axios from "axios"
 
-const createTask = () => {
-  
+const createTask = async (formData) => {
+  const response = await axios.post(`http://localhost:4000/task/add`,formData);
+  if(response.data.success){
+      console.log(response.data.message);
+  }else {
+      console.log("error",response.data.message);
+  }
 }
 </script>
 
