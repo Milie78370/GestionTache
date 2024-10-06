b19e7f657cefce0c3561e2990c02d6e7c9d6f506
Milie10 <elaye78370@gmail.com>
2024-10-06 17:41:20 +0200

    Formulaire pour les taches

---
+++ frontend/src/components/FormTask.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <h3>Créer une tâche</h3>
+    <form @submit.prevent="createTask">
+      <input type="text" placeholder="Libelle" v-model="libelle" ref="txtName" />
+      <input type="time" placeholder="heure de debut" v-model="heureDeb" />
+      <input type="time" placeholder="heure de fin" v-model="heureFin" />
+      <button :disabled="!isValidForm">Confirmer</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import { ref, computed, onMounted } from 'vue'
+export default {
+  setup(props, context) {
+    const libelle = ref('')
+    const heureDeb = ref('')
+    const heureFin = ref('')
+    let txtName = ref(null)
+
+    const isValidForm = computed(() => {
+      if (libelle.value !== '' && heureDeb.value !== '' && heureFin.value !== '') {
+        return true
+      }
+      return false
+    })
+
+    function resetForm() {
+      libelle.value = ''
+      heureDeb.value = ''
+      heureFin.value = ''
+    }
+
+    function createTask() {
+      const tasks = {
+        libelle: libelle.value,
+        heureDeb: heureDeb.value,
+        heureFin: heureFin.value
+      }
+      console.log(tasks)
+      context.emit('createTask', tasks)
+      resetForm()
+    }
+
+    onMounted(() => {
+      console.log('onMounted | txtName.value', txtName.value);
+      txtName.value.focus();
+    })
+
+    return {
+      libelle,
+      heureDeb,
+      heureFin,
+      isValidForm,
+      resetForm,
+      createTask,
+      txtName
+    }
+  }
+}
+</script>
+
+<style scoped>
+input,
+button {
+  width: 90%;
+  margin: 5px 10px;
+}
+</style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,15 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import HelloWorld from '../components/HelloWorld.vue'
+import Form from "../components/FormTask.vue"
+
+const createTask = () => {
+  
+}
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <HelloWorld msg="Créer une tâche"/>
+    <Form @createTask="createTask"/>
   </main>
 </template>
